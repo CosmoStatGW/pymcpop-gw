57afdd759cfb53034c4ece66d51fe5361de4ffa0
Michele <mancarell@gmail.com>
2025-05-12 13:17:28 +0200

    typo fix

--- pymcpop-gw/fit_model.py
+++ pymcpop-gw/fit_model.py
@@ -711,7 +711,7 @@ if __name__=='__main__':
     # Run sampler
     ################################################
 
-    if pymc.__version__.split('.')[1]>22: # recent versions of pymc
+    if pm.__version__.split('.')[1]>22: # recent versions of pymc
         sampler_kwargs = {
                     "draws": FLAGS.nsteps,
                     "tune":FLAGS.ntune,
