fix variance for marginal case; uniform-like mu priors for reparam mass

pymcpop-gw/fit_model_numpyro.py
@@ -775,7 +775,7 @@ def main():
 
     if FLAGS.mass_model=='DPLDP' and FLAGS.reparam_mass:
 
-        for drop in ["mb","u","v","sigma1","sigma2","delta_m1","delta_m2","alpha1","alpha2"]:
+        for drop in ["mb","u","v","mu1","mu2","sigma1","sigma2","delta_m1","delta_m2","alpha1","alpha2"]:
             init_vals.pop(drop, None)
         
         mb_a, mb_b = priors["mb"][0], priors["mb"][1]
@@ -789,6 +789,16 @@ def main():
         v0 = jm.unit_interval_sigmoid_raw_init(ivals.get("v"))
         if v0 is not None:
             init_vals["v_raw"] = v0
+
+        mu1a, mu1b = priors["mu1"][0], priors["mu1"][1]
+        mu10 = jm.bounded_sigmoid_raw_init(ivals.get("mu1"), mu1a, mu1b)
+        if mu10 is not None:
+            init_vals["mu1_raw"] = mu10
+        
+        mu2a, mu2b = priors["mu2"][0], priors["mu2"][1]
+        mu20 = jm.bounded_sigmoid_raw_init(ivals.get("mu2"), mu2a, mu2b)
+        if mu20 is not None:
+            init_vals["mu2_raw"] = mu20
 
 
         if "alpha_bar" not in init_vals:
@@ -1018,17 +1028,88 @@ def main():
     #mcmc.print_summary()
     samples = mcmc.get_samples(group_by_chain=True)
 
+    
 
-    idata = az.from_numpyro(mcmc)
-    az.to_netcdf(idata, os.path.join(FLAGS.fout, "trace.nc"))
+    ################################################
+    # Save and exit
+    ################################################
 
-    # also save raw samples in npz
-    np.savez(os.path.join(FLAGS.fout, "trace.npz"), **{k: np.asarray(v) for k, v in samples.items()})
+    
+    print( "\nDone." )
+    print("\nSaving trace...")
+    try:
+        idata = az.from_numpyro(mcmc)
+        tout = os.path.join(FLAGS.fout, "trace.nc")
+        az.to_netcdf(idata, tout)
+    
+        # also save raw samples in npz
+        np.savez(os.path.join(FLAGS.fout, "trace.npz"), **{k: np.asarray(v) for k, v in samples.items()})
 
-
+        print("✅ Trace saved in %s"%tout)
+    except Exception as e:
+        print(e)
+        print("⚠️ Saving failed !")
 
 
     
+    ################################################
+    # Plot
+    ################################################
+
+
+    print("\nMaking summary plots...")
+
+    
+    import matplotlib.pyplot as plt
+
+    vplot = dense_blocks[0]
+
+    try:
+        print("Plotting trace...")
+        az.plot_trace(trace, var_names = vplot, );
+        plt.savefig( os.path.join(FLAGS.fout, 'trace.pdf'), bbox_inches='tight')
+        plt.close()
+    except:
+        print('No trace plot produced')
+
+    try:
+        import corner
+        print("Plotting corner...")
+        _ = corner.corner(
+            trace,
+            var_names = vplot,
+            labels = vplot,  
+            color='darkred',
+            plot_points=False,
+            levels=[0.68, 0.90],
+            show_titles=True, 
+            title_kwargs={"fontsize": 15, }, label_kwargs={"fontsize": 15},
+            density=True,
+            smooth=0.9, 
+            fill_contours=True,
+             bins=20, 
+            title_fmt='.2f', 
+            hist_bin_factor=1,
+            quantiles=[0.05, 0.5, 0.95],
+    )
+    
+        plt.savefig( os.path.join(FLAGS.fout, 'corner_all.pdf'), bbox_inches='tight')
+        plt.close()
+    except:
+        print('No corner plot produced')
+
+    print("\nDone.")
+    #########
+
+    
+    print()
+    print('*'*80)
+    print('END. Results are saved in: %s'%FLAGS.fout)
+    print('*'*80)
+    print()
+
+    
+    myLog.close()
 
 
 

pymcpop-gw/jax_models.py
@@ -935,7 +935,7 @@ def make_model_jax(  priors,
                 beta_ = normal_from_bounds_95("beta", priors["beta"][0], priors["beta"][1] )
         
                 mb_a, mb_b = priors["mb"][0], priors["mb"][1]
-                mb_ = bounded_sigmoid("mb", mb_a, mb_b, raw_sigma=RAW_SD_95)
+                mb_ = bounded_sigmoid("mb", mb_a, mb_b, raw_sigma=1)
                 
                 
                 sigma1_          = floored_lognormal_q95("sigma1", priors["sigma1"][0], priors["sigma1"][1], median_frac=0.2)
@@ -943,19 +943,21 @@ def make_model_jax(  priors,
     
                 
                 
-                mu1_             = normal_from_bounds_95("mu1", priors["mu1"][0], priors["mu1"][1])
-                mu2_             = normal_from_bounds_95("mu2", priors["mu2"][0], priors["mu2"][1] )
-
+                # mu1_             = normal_from_bounds_95("mu1", priors["mu1"][0], priors["mu1"][1])
+                # mu2_             = normal_from_bounds_95("mu2", priors["mu2"][0], priors["mu2"][1] )
                 # just in case mu1 gets too small
-                numpyro.factor("mu1_neg_guard", jnp.where(mu1_ < 0.0, -jnp.inf, 0.0))
+                #numpyro.factor("mu1_neg_guard", jnp.where(mu1_ < 0.0, -jnp.inf, 0.0))
 
+                mu1_ = bounded_sigmoid("mu1", priors["mu1"][0], priors["mu1"][1], raw_sigma=1)
+                mu2_ = bounded_sigmoid("mu2", priors["mu2"][0], priors["mu2"][1], raw_sigma=1)
 
                 
-                u = unit_interval_sigmoid("u", raw_sigma=RAW_SD_95 )
+                
+                u = unit_interval_sigmoid("u", raw_sigma=1 )
                 m1_low_ = 3.0 + (10.0 - 3.0) * jnp.sqrt(u)
                 numpyro.deterministic("m1_low", m1_low_)
         
-                v = unit_interval_sigmoid("v",raw_sigma=RAW_SD_95)
+                v = unit_interval_sigmoid("v",raw_sigma=1)
                 m2_low_ = 3.0 + v * (m1_low_ - 3.0)
                 numpyro.deterministic("m2_low", m2_low_)
         
@@ -965,8 +967,8 @@ def make_model_jax(  priors,
                 delta_m1_ = floored_lognormal_q95("delta_m1", priors["delta_m1"][0], priors["delta_m1"][1], median_frac=0.2 )
                 delta_m2_ = floored_lognormal_q95("delta_m2", priors["delta_m2"][0], priors["delta_m2"][1], median_frac=0.2 )
         
-                numpyro.deterministic("m1_taper_end", m1_low_ + delta_m1_)
-                numpyro.deterministic("m2_taper_end", m2_low_ + delta_m2_)
+                #numpyro.deterministic("m1_taper_end", m1_low_ + delta_m1_)
+                #numpyro.deterministic("m2_taper_end", m2_low_ + delta_m2_)
 
             else:
                 alpha1_ = numpyro.sample("alpha1", dist.Uniform(priors["alpha1"][0], priors["alpha1"][1]))