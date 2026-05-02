bd9f4f200cea28adb8c4da59424025cd27e0cbc7
Michele <mancarell@gmail.com>
2026-05-02 13:33:36 +0200

    residual cleaning

--- pymcpop-gw/constants.py
+++ pymcpop-gw/constants.py
@@ -1,4 +1,3 @@
-import pytensor.tensor as at
 import pade_cosmo as pc
 import numpy as np
 import jax.numpy as jnp
@@ -34,12 +33,6 @@ def gauss_legendre_01(n=32, ): #dtype="float64"):
 _x01_np, _w01_np = gauss_legendre_01(n=64)  # 16–64 usually plenty
 
 
-x01_at = at.as_tensor_variable(_x01_np)     # shape (n,)
-w01_at = at.as_tensor_variable(_w01_np)     # shape (n,)
-
-_x01_at = x01_at
-_w01_at = w01_at
-
 
 
 # ---------------------------------------------------------------------
@@ -58,7 +51,6 @@ p, q = pc.flat_wcdm_pade_coefficients(w0=-1.0, zpower=0)  # arrays of floats
 max_m = 500.0
 
 _tgrid_np = np.linspace(0.0, 1.0, 500).astype("float64")
-_tgrid_at = at.as_tensor_variable(_tgrid_np)  # if you really want stop_grad, do it where you build graphs
 
 def _get_t_grid():
     return _tgrid_at
